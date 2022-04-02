8dcfbd0f92ab45f4c4641961fe03578cfffeb8f8
Adrian Kokot <splateusz@gmail.com>
2022-04-02 15:23:09 +0200

    Finish operators functionality

--- RpnCalc/app/src/main/java/com/kokotadrian/rpncalc/MainActivity.kt
+++ RpnCalc/app/src/main/java/com/kokotadrian/rpncalc/MainActivity.kt
@@ -2,8 +2,11 @@ package com.kokotadrian.rpncalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.children
 import com.kokotadrian.rpncalc.databinding.ActivityMainBinding
@@ -12,120 +15,161 @@ import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var stackTexts: Array<TextView>
     private lateinit var binding: ActivityMainBinding
-
-    private var currentValue = "";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-//        val containers =
-//            arrayOf(
-//                binding.calcFirstRow,
-//                binding.calcSecondRow,
-//                binding.calcThirdRow,
-//                binding.calcFourthRow
-//            )
-//
-//        val actionIds = arrayOf(
-//            R.id.actionButtonBack,
-//            R.id.actionButtonEnter,
-//            R.id.actionButtonMinus,
-//            R.id.actionButtonMultiply,
-//            R.id.actionButtonPlus
-//        );
-//
-//        for (container in containers) {
-//            for (i in 0..container.childCount) {
-//                val child = container.getChildAt(i)
-//
-//                if (child is Button && !actionIds.contains(child.id)) {
-//                    child.setOnClickListener() {
-//                        currentValue += child.text;
-//                        binding.statusText.text = currentValue;
-//                    }
-//                }
-//            }
-//        }
-//
-//        binding.actionButtonBack.setOnClickListener() {
-//            currentValue = currentValue.dropLast(1)
-//            binding.statusText.text = currentValue;
-//        }
+        stackTexts = arrayOf(
+            binding.stackValue1,
+            binding.stackValue2,
+            binding.stackValue3,
+            binding.stackValue4
+        )
+
+        setContentView(binding.root)
     }
 
     private var canAddDot = true
     private var canAddNumber = true
-    private var canAddOperator = false
 
     private var stack = ArrayDeque<Double>()
 
     fun onNumberButtonClick(view: View) {
         if (view is Button) {
             if (canAddNumber) {
-                binding.statusText.append(view.text);
-                canAddOperator = false
+                appendToCalcTextField(view.text);
             }
         }
     }
 
     fun onDotButtonClick(view: View) {
         if (canAddDot) {
-            binding.statusText.append(".");
+            binding.calcText.append(".");
             canAddDot = false
         }
     }
 
     fun onBackButtonClick(view: View) {
-        binding.statusText.text = binding.statusText.text.dropLast(1)
-        canAddDot = !binding.statusText.text.contains(".");
-        canAddOperator = binding.statusText.text.isEmpty() && stack.size >= 2
+        calcTextField = calcTextField.dropLast(1)
+        canAddDot = !calcTextField.contains(".")
     }
 
     fun onEnterButtonClick(view: View) {
-        if (binding.statusText.text.isNotEmpty()) {
-            val newNumber = binding.statusText.text.toString().toDouble()
-            pushOnStack(newNumber)
-            binding.statusText.text = ""
-
-            canAddOperator = stack.size >= 2
+        if (calcTextField.isNotEmpty()) {
+            pushOnStack(calcTextFieldValue)
+            resetCalcTextFieldValue()
+            canAddDot = true
         }
     }
 
+    private fun appendToCalcTextField(value: CharSequence) {
+        binding.calcText.append(value);
+    }
+
+    private fun appendToCalcTextField(value: String) {
+        binding.calcText.append(value);
+    }
+
+    private val calcTextFieldValue: Double
+        get() = calcTextField.toDouble()
+
+    private fun resetCalcTextFieldValue() {
+        calcTextField = ""
+    }
+
+    private var calcTextField: String
+        get() = binding.calcText.text.toString()
+        set(value) {
+            binding.calcText.text = value
+        }
+
+
     fun onOperatorButtonClick(view: View) {
         if (view is Button) {
-            if (canAddOperator) {
-                val num1 = stack.pop()
-                val num2 = stack.pop()
 
-                val result: Double = when (view.id) {
-                    R.id.actionButtonPlus -> num1 + num2
-                    R.id.actionButtonMinus -> num2 - num1
-                    R.id.actionButtonDivide -> num2 / num1
-                    R.id.actionButtonMultiply -> num1 * num2
-                    R.id.actionButtonSqrt -> {
-                        stack.push(num2)
-                        sqrt(num1)
-                    }
-                    R.id.actionButtonPower -> Math.pow(num1, num2)
+            val isSingleArgumentOperator = view.id == R.id.actionButtonSqrt
+
+            val result: Double
+
+            if (isSingleArgumentOperator) {
+
+                val value = if (calcTextField.isEmpty()) stack.pop() else calcTextFieldValue
+
+                result = when (view.id) {
+                    R.id.actionButtonSqrt -> sqrt(value)
                     else -> 0.0
                 }
 
-                pushOnStack(result)
-                binding.statusText.text = result.toString()
+            } else {
+
+                if (calcTextField.isEmpty() && stack.size < 2 || calcTextField.isNotEmpty() && stack.size == 0) {
+                    return
+                }
+
+                val values = arrayOf(
+                    (if (calcTextField.isEmpty()) stack.pop() else calcTextFieldValue),
+                    stack.pop()
+                )
+
+                result = when (view.id) {
+                    R.id.actionButtonPlus -> values[1] + values[0]
+                    R.id.actionButtonMinus -> values[1] - values[0]
+                    R.id.actionButtonDivide -> {
+                        if (values[0] == 0.0) {
+                            val toast =
+                                Toast.makeText(this, "You cannot divide by 0", Toast.LENGTH_LONG)
+                            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+                            toast.show()
+
+                            values[1]
+                        } else {
+                            values[1] / values[0]
+                        }
+                    }
+                    R.id.actionButtonMultiply -> values[1] * values[0]
+                    R.id.actionButtonPower -> Math.pow(values[1], values[0])
+                    else -> 0.0
+                }
+
             }
+
+            pushOnStack(result)
+            resetCalcTextFieldValue()
         }
     }
 
     fun onChangeSignButtonClick(view: View) {}
     fun onDropButtonClick(view: View) {}
     fun onSwapButtonClick(view: View) {}
-    fun onACButtonClick(view: View) {}
 
-    fun pushOnStack(value: Double) {
+    fun onACButtonClick(view: View) {
+        stack.clear()
+        resetCalcTextFieldValue()
+        renderStackValues()
+    }
+
+    private var precisionDigits = 8
+
+    private fun renderStackValues() {
+
+        val stackTextViewCount = stackTexts.size
+
+        val stackSize = if (stack.size >= stackTextViewCount) stackTextViewCount else stack.size
+
+        val stackValues = stack.take(stackSize).toTypedArray()
+
+        for (v in 0 until stackTextViewCount) {
+            stackTexts[v].text =
+                if (v >= stackSize) "" else "%.${precisionDigits}f".format(stackValues[v]).trimEnd('0', ',', '.')
+        }
+    }
+
+    private fun pushOnStack(value: Double) {
         stack.push(value)
+        renderStackValues()
     }
 }
